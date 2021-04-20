82d74a29ab2a9bfba387a5e97e540f1df43e6a12
nuke <rochadcmarcos@gmail.com>
2021-04-20 07:09:16 -0300

    getting conmebol_points from csv

--- ranking.py
+++ ranking.py
@@ -11,3 +11,19 @@ with open('ranking_conmebol.csv') as file:
         del line[-1]
         line.append(val)
         print(line)
+
+def define_conmebol_points(clubs):
+    with open('ranking_conmebol.csv') as file:
+        lines = file.readlines() 
+        for club in clubs:
+
+            for line in lines:
+                line = line.split(',')
+                if line[0] == club.name:
+                
+                    val = line[-1] 
+                    val.replace('\n', '')
+                    val = float(val)
+
+                    club.ranking_points += val
+
