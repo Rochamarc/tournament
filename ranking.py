ddb9d1c6bb9528331b185f2deaf48f2b55e276f4
nuke <rochadcmarcos@gmail.com>
2021-04-20 09:35:06 -0300

    refactor: ranking algor

--- ranking.py
+++ ranking.py
@@ -1,27 +1,6 @@
 import csv 
 import sqlite3
 
-with open('ranking_conmebol.csv') as file:
-    lines = file.readlines() # isso é uma lista de strings
-
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    for line in lines:
-        line = line.split(',')
-        val = line[-1] 
-        val.replace('\n', '')
-        val = float(val)
-        del line[-1]
-        line.append(val)
-
-        cursor.execute("""
-        INSERT INTO clubs_ranking (name, country, points) VALUES (?,?,?)
-        """, line)
-
-        conn.commit()
-    conn.close()    
-
 def define_conmebol_points(clubs):
     with open('ranking_conmebol.csv') as file:
         lines = file.readlines() 
@@ -37,3 +16,28 @@ def define_conmebol_points(clubs):
 
                     club.ranking_points += val
 
+
+if __name__ == "__main__":
+        
+    with open('ranking_conmebol.csv') as file:
+        lines = file.readlines() # isso é uma lista de strings
+    
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+    
+        for line in lines:
+            line = line.split(',')
+            val = line[-1] 
+            val.replace('\n', '')
+            val = float(val)
+            del line[-1]
+            line.append(val)
+    
+            cursor.execute("""
+            INSERT INTO clubs_ranking (name, country, points) VALUES (?,?,?)
+            """, line)
+    
+            conn.commit()
+        conn.close()    
+    
+    
