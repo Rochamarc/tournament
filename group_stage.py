22af5bc03c568c16d51735efc2d75b74144375c8
nuke <rochadcmarcos@gmail.com>
2021-05-06 05:16:36 -0300

    update: game() to Game()

--- group_stage.py
+++ group_stage.py
@@ -1,11 +1,19 @@
-from game_match import Game
+from game import Game
 import pandas as pd 
 from time import sleep 
 from ranking import update_player_stats
+from classes import *
+
+maraca = Stadium('Maracana', 'Rio de Janeiro, Brasil') # stadium test
+
 
 def group_stage(group, group_list, competition, verbose=False):
-    """ Retorna um dicionario """
+    ''' Simulates the whole group phase from a single group
+        Return a dict return { 'group': <class 'str'>, 'first place': <class 'club()'> , 'second place': <class 'club()'> }
+    '''
     club_one, club_two, club_three, club_four = group_list[0], group_list[1], group_list[2], group_list[3]
+
+    competition = 'Conmebol Libertadores'
 
     # Gerando as seis partidas
     for i in range(1,7):
@@ -23,44 +31,48 @@ def group_stage(group, group_list, competition, verbose=False):
         # Rodada 1 de 6
         if i == 1:
             # club_one x club_two
-            Game.game_match(i, competition, club_one,club_two)
+            #Game.game_match(i, competition, club_one,club_two)
+            Game(club_one, club_two, competition, i, maraca).start() # club_one.stadium
             # club_three x club_four
-            Game.game_match(i, competition, club_three,club_four)
+            Game(club_three, club_four, competition, i, maraca).start()
+            #Game.game_match(i, competition, club_three,club_four)
             if verbose:
                 sleep(2)
         elif i == 2:
             # club_one x club_four
-            Game.game_match(i, competition, club_one,club_four)
+            Game(club_one, club_four, competition, i, maraca).start()
+            #Game.game_match(i, competition, club_one,club_four)
             # club_two x club_three
-            Game.game_match(i, competition, club_two,club_three)
+            Game(club_two, club_three, competition, i, maraca).start()
+            #Game.game_match(i, competition, club_two,club_three)
             if verbose:
                 sleep(2)
         elif i == 3:
             # dotmund x club_one
-            Game.game_match(i, competition, club_three,club_one)
+            Game(club_three,club_one, competition, i, maraca).start() 
             # club_four x club_two
-            Game.game_match(i, competition, club_four,club_two)
+            Game(club_four,club_two, competition, i, maraca).start() 
             if verbose:
                 sleep(2)
         elif i == 4:
             # club_two x club_one
-            Game.game_match(i, competition, club_two,club_one)
+            Game(club_two,club_one, competition, i, maraca).start() 
             # club_four x club_three
-            Game.game_match(i, competition, club_four,club_three)
+            Game(club_four,club_three, competition, i, maraca).start() 
             if verbose:
                 sleep(2)
         elif i == 5:
             # club_four x club_one
-            Game.game_match(i, competition, club_four,club_one)
+            Game(club_four,club_one, competition, i, maraca).start() 
             # dormtund x club_two 
-            Game.game_match(i, competition, club_three,club_two)
+            Game(club_three,club_two, competition, i, maraca).start() 
             if verbose:
                 sleep(2)
         elif i == 6:
             # club_one x club_three
-            Game.game_match(i, competition, club_one,club_three)
+            Game(club_one,club_three, competition, i, maraca).start() 
             # club_two x club_four
-            Game.game_match(i, competition, club_two,club_four)
+            Game(club_two,club_four, competition, i, maraca).start() 
             if verbose:
                 sleep(2)
 
