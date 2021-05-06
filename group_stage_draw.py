023af9fa08e859ebae41f4ca67716f7f299e6754
nuke <rochadcmarcos@gmail.com>
2021-05-06 05:13:00 -0300

    fix: index error

--- group_stage_draw.py
+++ group_stage_draw.py
@@ -10,130 +10,132 @@ from pprint import pprint
 def define_group_stage(clubs_sorted, qualifying_phase, verbose=False):
     """ Lista de clubes, lista de nomes dos qualificados na pré e um argumento verbose opicional """
     
-    # sorteados pelo ranking
-    clubs_sorted.sort(key=lambda club : club.ranking_points, reverse=True) 
+    ex_club_sorted = clubs_sorted.copy()
+    ex_qualifyin = qualifying_phase.copy()
 
-    for club in clubs_sorted:
-        temp = None 
+    try:
+        # sorteados pelo ranking
+        clubs_sorted.sort(key=lambda club : club.ranking_points, reverse=True) 
 
-        for quali in qualifying_phase:
-            if quali == club.name:
-                temp = club 
-                clubs_sorted.remove(temp)
-                clubs_sorted.append(temp) # add no final da lista
+        for club in clubs_sorted:
+            temp = None 
 
-    pots = {
-        "1": clubs_sorted[0:8],
-        "2": clubs_sorted[8:16],
-        "3": clubs_sorted[16:24],
-        "4": clubs_sorted[24:32]
-    }
+            for quali in qualifying_phase:
+                if quali == club.name:
+                    temp = club 
+                    clubs_sorted.remove(temp)
+                    clubs_sorted.append(temp) # add no final da lista
 
-    pprint(pots)
+        pots = {
+            "1": clubs_sorted[0:8],
+            "2": clubs_sorted[8:16],
+            "3": clubs_sorted[16:24],
+            "4": clubs_sorted[24:32]
+        }
 
-    groups = {
-        "A" : [],
-        "B" : [],
-        "C" : [],
-        "D" : [],
-        "E" : [],
-        "F" : [],
-        "G" : [],
-        "H" : []
-    }
+        pprint(pots)
 
-    # sorting pot 1
-    for club in pots["1"]:
-        print("Sorteando o pote 1")
-        group_choices = ["A","B","C","D","E", "F", "G", "H"]
+        groups = {
+            "A" : [],
+            "B" : [],
+            "C" : [],
+            "D" : [],
+            "E" : [],
+            "F" : [],
+            "G" : [],
+            "H" : []
+        }
 
-        for group, group_clubs in groups.items():
-            if len(group_clubs) == 1:
-                group_choices.remove(group)
+        # sorting pot 1
+        for club in pots["1"]:
+            print("Sorteando o pote 1")
+            group_choices = ["A","B","C","D","E", "F", "G", "H"]
 
-        group_sorted = choice(group_choices)
+            for group, group_clubs in groups.items():
+                if len(group_clubs) == 1:
+                    group_choices.remove(group)
+
+            group_sorted = choice(group_choices)
+
+            if verbose:
+                print(f"{club} foi sorteado para o grupo {group_sorted}")
+                sleep(2) 
+            groups[group_sorted].append(club)
 
         if verbose:
-            print(f"{club} foi sorteado para o grupo {group_sorted}")
-            sleep(2) 
-        groups[group_sorted].append(club)
-
-    if verbose:
-        pprint(groups)
-        sleep(2)
-
-    # sorting pot 2
-    for club in pots["2"]:
-        print("Sorteando o pote 2")
-        group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
-
-
-        for group, group_clubs in groups.items():
-            if len(group_clubs) == 2:
-                group_choices.remove(group)
-            else:
-                for group_club in group_clubs:
-                    if group_club.country == club.country:
-                        group_choices.remove(group)
-
-
-        group_sorted = group_choices[0] #choice(group_choices)
-        
-        if verbose:
-            print(f"{club} foi sorteado para o grupo {group_sorted}")
+            pprint(groups)
             sleep(2)
 
-        groups[group_sorted].append(club)
+        # sorting pot 2
+        for club in pots["2"]:
+            print("Sorteando o pote 2")
+            group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
-    if verbose:
-        pprint(groups)
-        sleep(2)
+
+            for group, group_clubs in groups.items():
+                if len(group_clubs) == 2:
+                    group_choices.remove(group)
+                else:
+                    for group_club in group_clubs:
+                        if group_club.country == club.country:
+                            group_choices.remove(group)
+
+
+            group_sorted = group_choices[0] #choice(group_choices)
+            
+            if verbose:
+                print(f"{club} foi sorteado para o grupo {group_sorted}")
+                sleep(2)
+
+            groups[group_sorted].append(club)
+
+        if verbose:
+            pprint(groups)
+            sleep(2)
+        
+        # sorting pot 3
+        for i in range(len(pots["3"])):
+            print("Sorteando o pote 3")
+            group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
+
+            club = choice(pots["3"])
+            pots["3"].remove(club)
+
+            for group, group_clubs in groups.items():
+                if len(group_clubs) == 3:
+                    group_choices.remove(group)
+                else:
+                    for group_club in group_clubs:
+                        if group_club.country == club.country:
+                            group_choices.remove(group)
+            
+
+            group_sorted = group_choices[0] # choice(group_choices)
+
+            if verbose:
+                print(f"{club} foi sorteado para o grupo {group_sorted}")
+                sleep(2)
+
+            groups[group_sorted].append(club)        
+
+        # sorting pot 4
+        for club in pots["4"]:
+            print("Sorteando o pote 4")
+            group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
+
+            for group, group_clubs in groups.items():
+                if len(group_clubs) == 4:
+                    group_choices.remove(group)
+
+            group_sorted = choice(group_choices)
+            
+            if verbose:
+                print(f"{club} foi sorteado para o grupo {group_sorted}")
+                sleep(2)
+
+            groups[group_sorted].append(club)
     
-    # sorting pot 3
-    for i in range(len(pots["3"])):
-        print("Sorteando o pote 3")
-        group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
-
-        club = choice(pots["3"])
-        pots["3"].remove(club)
-
-        for group, group_clubs in groups.items():
-            if len(group_clubs) == 3:
-                group_choices.remove(group)
-            else:
-                for group_club in group_clubs:
-                    if group_club.country == club.country:
-                        group_choices.remove(group)
-        
-
-        group_sorted = group_choices[0] # choice(group_choices)
-
-        if verbose:
-            print(f"{club} foi sorteado para o grupo {group_sorted}")
-            sleep(2)
-
-        groups[group_sorted].append(club)        
-
-    if verbose:
-        pprint(groups)
-        sleep(2)
-
-    # sorting pot 4
-    for club in pots["4"]:
-        print("Sorteando o pote 4")
-        group_choices = ["A", "B", "C", "D", "E", "F", "G", "H"]
-
-        for group, group_clubs in groups.items():
-            if len(group_clubs) == 4:
-                group_choices.remove(group)
-
-        group_sorted = choice(group_choices)
-        
-        if verbose:
-            print(f"{club} foi sorteado para o grupo {group_sorted}")
-            sleep(2)
-
-        groups[group_sorted].append(club)
-    
+    except:
+        define_group_stage(ex_club_sorted, ex_qualifyin)
 
     return groups
