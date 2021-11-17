042a39aa9a4f7a906a55ec78002361f7fa2c99a6
nuke <rochadcmarcos@gmail.com>
2021-11-17 14:06:07 -0300

    update table method and remove upload_ranking_db

--- ranking.py
+++ ranking.py
@@ -1,6 +1,8 @@
 import csv 
 import sqlite3
-import pandas as pd 
+import pandas as pd
+
+from database import get_domestic_cup_table 
 
 
 class Ranking:
@@ -18,33 +20,7 @@ class Ranking:
                         val.replace('\n', '')
                         val = float(val)
 
-                        club.ranking_points += val
-
-    @staticmethod 
-    def upload_ranking_db():
-        print("Inserindo ranking da conmebol na base de dados!")
-        with open('ranking_conmebol.csv') as file:
-            lines = file.readlines() # isso é uma lista de strings
-        
-            conn = sqlite3.connect('database.db')
-            cursor = conn.cursor()
-        
-            for line in lines:
-                line = line.split(',')
-                val = line[-1] 
-                val.replace('\n', '')
-                val = float(val)
-                del line[-1]
-                line.append(val)
-        
-                cursor.execute("""
-                INSERT INTO clubs_ranking (name, country, points) VALUES (?,?,?)
-                """, line)
-        
-                conn.commit()
-
-            print("Ranking da conmebol inserido com sucesso!")
-            conn.close()    
+                        club.ranking_points += val    
 
     @staticmethod
     def individual_leaderboard(all_clubs, category, season, save_file=None):
@@ -72,13 +48,13 @@ class Ranking:
         return sorted_data_frame[:10]
 
     @staticmethod 
-    def table(clubs_data):
+    def table(season):
         ''' 
         Return a panda series with the tables below
         Position    Club    Matches    Won  Draw    Lost   Goals For    Goals Away    Goals Diff    Points
         '''
-        # statistics
-        stats = ['Points', 'Won', 'GD', 'GF']
-        df = pd.DataFrame(clubs_data, columns=['Club','Matches','Won','Draw','Lost','GF','GA','GD','Points'])
-        sorted_data_frame = df.sort_values(by=stats, axis=0, ascending=False) # Sort the data
-        return sorted_data_frame
+        value = get_domestic_cup_table(season)
+        
+        df = pd.DataFrame(value, index=None, columns=['id','Club','Matches','Won','Draw','Lost','GF','GA','GD','Points'])
+        
+        return df
