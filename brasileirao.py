d18d5ebc6f1f67554ea150b578702420338ccf0e
nuke <rochadcmarcos@gmail.com>
2021-12-24 19:06:12 -0300

    running more seasons

--- brasileirao.py
+++ brasileirao.py
@@ -14,107 +14,117 @@ p_data = PlayerData()
 stadiums = [ gene.reconstruct_stadiums() ]
 season = '2021'
 
-### SERIE A ###
-serie_a_matches = []
 
-competition_name = "Campeonato Brasileiro Série A"
-division = 'serie_a'
-
-
-serie_a_clubs = gene.reconstruct_clubs(division, season)
-
-schedule = gene.define_schedule(serie_a_clubs, stadiums[0]) # the schedule
-
-for club in serie_a_clubs:
-    club.set_formation(p_data.get_players(club.name))
-
-for rnd, game_info in schedule.items():
-    ''' Defining the schedule '''
-    for match in game_info:
-        print(match[0].start_eleven)
-        serie_a_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
+for i in range(4):
+    
+    if season != '2021':
+        gene.promotions_and_relegations(season) # initiate a new season
 
 
-tb = rk.domestic_table(division, season) # Get the initial domestic cup table
-print(tb) 
 
-for match in serie_a_matches:
-    ''' execute the matches '''
-    r = match.start()
-    match.save_game_database()
-    league.update_domestic_table(r['home_team'], division, season)
-    league.update_domestic_table(r['away_team'], division, season)
+    ### SERIE A ###
+    serie_a_matches = []
+
+    competition_name = "Campeonato Brasileiro Série A"
+    division = 'serie_a'
 
 
-print(league.get_domestic_cup_table(division, season))
-### END SERIE A ###
+    serie_a_clubs = gene.reconstruct_clubs(division, season)
 
-### SERIE B ###
-serie_b_matches = []
+    schedule = gene.define_schedule(serie_a_clubs, stadiums[0]) # the schedule
 
-competition_name = "Campeonato Brasileiro Série B"
-division = 'serie_b'
+    for club in serie_a_clubs:
+        club.set_formation(p_data.get_players(club.name))
 
-
-serie_b_clubs = gene.reconstruct_clubs(division, season)
-
-schedule = gene.define_schedule(serie_b_clubs, stadiums[0]) # the schedule
-
-for club in serie_b_clubs:
-    club.set_formation(p_data.get_players(club.name))
-
-for rnd, game_info in schedule.items():
-    ''' Defining the schedule '''
-    for match in game_info:
-        print(match[0].start_eleven)
-        serie_b_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
+    for rnd, game_info in schedule.items():
+        ''' Defining the schedule '''
+        for match in game_info:
+            print(match[0].start_eleven)
+            serie_a_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
 
 
-tb = rk.domestic_table(division, season) # Get the initial domestic cup table
-print(tb) 
+    tb = rk.domestic_table(division, season) # Get the initial domestic cup table
+    print(tb) 
 
-for match in serie_b_matches:
-    ''' execute the matches '''
-    r = match.start()
-    match.save_game_database()
-    league.update_domestic_table(r['home_team'], division, season)
-    league.update_domestic_table(r['away_team'], division, season)
-
-
-print(league.get_domestic_cup_table(division, season))
-### END SERIE B ###
-
-### SERIE C ###
-serie_c_matches = []
-
-competition_name = "Campeonato Brasileiro Série C"
-division = 'serie_c'
+    for match in serie_a_matches:
+        ''' execute the matches '''
+        r = match.start()
+        match.save_game_database()
+        league.update_domestic_table(r['home_team'], division, season)
+        league.update_domestic_table(r['away_team'], division, season)
 
 
-serie_c_clubs = gene.reconstruct_clubs(division, season)
+    print(league.get_domestic_cup_table(division, season))
+    ### END SERIE A ###
 
-schedule = gene.define_schedule(serie_c_clubs, stadiums[0]) # the schedule
+    ### SERIE B ###
+    serie_b_matches = []
 
-for club in serie_c_clubs:
-    club.set_formation(p_data.get_players(club.name))
-
-for rnd, game_info in schedule.items():
-    ''' Defining the schedule '''
-    for match in game_info:
-        print(match[0].start_eleven)
-        serie_c_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
+    competition_name = "Campeonato Brasileiro Série B"
+    division = 'serie_b'
 
 
-tb = rk.domestic_table(division, season) # Get the initial domestic cup table
-print(tb) 
+    serie_b_clubs = gene.reconstruct_clubs(division, season)
 
-for match in serie_c_matches:
-    ''' execute the matches '''
-    r = match.start()
-    match.save_game_database()
-    league.update_domestic_table(r['home_team'], division, season)
-    league.update_domestic_table(r['away_team'], division, season)
+    schedule = gene.define_schedule(serie_b_clubs, stadiums[0]) # the schedule
+
+    for club in serie_b_clubs:
+        club.set_formation(p_data.get_players(club.name))
+
+    for rnd, game_info in schedule.items():
+        ''' Defining the schedule '''
+        for match in game_info:
+            print(match[0].start_eleven)
+            serie_b_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
 
 
-print(league.get_domestic_cup_table(division, season))
-### END SERIE C ###
+    tb = rk.domestic_table(division, season) # Get the initial domestic cup table
+    print(tb) 
+
+    for match in serie_b_matches:
+        ''' execute the matches '''
+        r = match.start()
+        match.save_game_database()
+        league.update_domestic_table(r['home_team'], division, season)
+        league.update_domestic_table(r['away_team'], division, season)
+
+
+    print(league.get_domestic_cup_table(division, season))
+    ### END SERIE B ###
+
+    ### SERIE C ###
+    serie_c_matches = []
+
+    competition_name = "Campeonato Brasileiro Série C"
+    division = 'serie_c'
+
+
+    serie_c_clubs = gene.reconstruct_clubs(division, season)
+
+    schedule = gene.define_schedule(serie_c_clubs, stadiums[0]) # the schedule
+
+    for club in serie_c_clubs:
+        club.set_formation(p_data.get_players(club.name))
+
+    for rnd, game_info in schedule.items():
+        ''' Defining the schedule '''
+        for match in game_info:
+            print(match[0].start_eleven)
+            serie_c_matches.append(Game(match[0], match[1], competition_name, int(rnd.split(' ')[-1]), season, head_stadium=match[-1]))
+
+
+    tb = rk.domestic_table(division, season) # Get the initial domestic cup table
+    print(tb) 
+
+    for match in serie_c_matches:
+        ''' execute the matches '''
+        r = match.start()
+        match.save_game_database()
+        league.update_domestic_table(r['home_team'], division, season)
+        league.update_domestic_table(r['away_team'], division, season)
+
+
+    print(league.get_domestic_cup_table(division, season))
+    ### END SERIE C ###
+
+    season = str(int(season) + 1) # next season
