4a6eee41d34cfc359dc78f2d2be65453787ec243
nuke <rochadcmarcos@gmail.com>
2021-12-30 23:24:29 -0300

    player stats database update fixed

--- database.py
+++ database.py
@@ -449,10 +449,10 @@ class PlayerData():
 
             cursor.execute(f"""
                 UPDATE players 
-                SET matches_played=matches_played+?, 
-                    goals=goals+?,
-                    assists=assists+?,
-                    points=points+?
+                SET matches_played=?, 
+                    goals=?,
+                    assists=?,
+                    points=?
                 WHERE id = ?
             """, player_data)
 
