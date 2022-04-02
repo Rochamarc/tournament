f811f362d97d9343e546aec8720c15e7b3648c9a
nuke <rochadcmarcos@gmail.com>
2022-04-02 20:55:24 -0300

    file system add to script

--- script.sh
+++ script.sh
@@ -4,5 +4,8 @@ echo "Criando diretorios necessarios!"
 mkdir files
 mkdir files/saves
 mkdir files/names
+mkdir files/brasileirao/serie\ a/2021
+mkdir files/brasileirao/serie\ b/2021
+mkdir files/brasileirao/serie\ c/2021
 echo "Criando e configurando base de dados!"
 python3 database.py
