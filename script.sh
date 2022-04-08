df9d5fe579faed66f8fae6d54fc6058e1fe8d6b0
nuke <rochadcmarcos@gmail.com>
2022-04-08 01:19:30 -0300

    remove directory creating from sh

--- script.sh
+++ script.sh
@@ -4,8 +4,5 @@ echo "Criando diretorios necessarios!"
 mkdir files
 mkdir files/saves
 mkdir files/names
-mkdir files/brasileirao/serie\ a/2021
-mkdir files/brasileirao/serie\ b/2021
-mkdir files/brasileirao/serie\ c/2021
 echo "Criando e configurando base de dados!"
 python3 database.py
