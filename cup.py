616c2f1640cd9460f1862ab3a8377c323c10cf11
nuke <rochadcmarcos@gmail.com>
2022-09-22 15:16:34 -0300

    cup season class

--- cup.py
+++ cup.py
@@ -1,28 +1,37 @@
-from random import choice
 from database import PlayerData
-
-from draw import Draw
-from knock_out import KnockOutGame
 from classes_helper import GenerateClass
-
-draw = Draw()
-generate = GenerateClass()
-
-copa_do_brasil_matches = draw.national_draw()
-
-stadiums = generate.reconstruct_stadiums() 
+from knock_out import KnockOutGame
+from random import choice 
 
 player_data = PlayerData()
+generate = GenerateClass()
 
-results = []
+class Cup:
+    def __init__(self, competition_name, season, phase, matches):
+        self.competition_name = competition_name
+        self.season = season
+        self.phase = phase 
 
-for match in copa_do_brasil_matches:
-    match[0].set_formation(player_data.get_players(match[0].name))
-    match[1].set_formation(player_data.get_players(match[1].name))
+        self.matches = matches
+
+        self.stadiums = generate.reconstruct_stadiums() 
     
-    m_1 = KnockOutGame(match[0], match[1], 'Copa do Brasil', 1, '2021', 'Knock Out', head_stadium=choice(stadiums), verbose=False).first_leg()
-    print(m_1)
+    def run(self):
+        results = []
 
-    results.append(m_1)
+        for match in self.matches:
+            match[0].set_formation(player_data.get_players(match[0].name))
+            match[1].set_formation(player_data.get_players(match[1].name))
+            m_1 = KnockOutGame(match[0], match[1], 'Copa do Brasil', 1, '2021', 'First Phase', head_stadium=choice(self.stadiums), verbose=False).first_leg()
+            results.append(m_1)
+        
+        return [ i['winner'] for i in results ]
 
-print(results)
+    def show(self):
+        print(f"{self.competition_name.upper()}")
+        print(f"{self.season}")
+        print(f"{self.phase}\n")
+        for i in self.matches:
+            print("="*50)
+            print(f"{i[0]} x {i[1]}")
+            print("="*50)
