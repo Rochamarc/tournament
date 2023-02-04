83e27f90d7694395e8180b0e5d0c67d3ba919f42
nuke <rochadcmarcos@gmail.com>
2023-02-04 02:49:12 -0300

    passing database with flags `reset_database`

--- reset_database.sh
+++ reset_database.sh
@@ -4,6 +4,6 @@
 echo "Reseting database"
 rm -r database.db 
 echo "Recreating Base Database"
-python3 database.py
+python3 database.py $1
 echo "Recretating International Database"
 python3 setting_international_clubs.py 
