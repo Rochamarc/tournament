962fb9f5ae37f4dd32d313181e002c7a8fe35955
nuke <rochadcmarcos@gmail.com>
2023-02-04 21:22:54 -0300

    move ranking_conmebol to files/conmebol

--- db/database.py
+++ db/database.py
@@ -134,7 +134,7 @@ def upload_ranking_db(verbose=False):
     
     print("Inserindo ranking da conmebol na base de dados!")
     
-    with open('ranking_conmebol.csv', encoding='utf8') as file:
+    with open('files/conmebol/ranking_conmebol.csv', encoding='utf8') as file:
         lines = file.readlines() 
     
         conn = sqlite3.connect(database)
