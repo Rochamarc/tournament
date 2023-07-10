5418b412e637fa8fba2c196ae46a0d45cf68a3d7
karkraz <rochadcmarcos@gmail.com>
2023-07-10 15:38:29 -0300

    add query_helper

---
+++ db/open_query.py
@@ -0,0 +1,16 @@
+import os 
+
+class QueryHelper:
+    @staticmethod
+    def open_query(query: str) -> str:
+        ''' Prepare and return the string for the query '''
+        s = None
+        with open(f'query/create_{query}', 'r') as f:
+            s = f.readlines()[0].replace('\n', '')
+
+        return s
+
+    @staticmethod 
+    def create_query() -> None:
+        ''' Creeate a query (migration) to the database '''
+        pass 
