329e4ee1b5f3d388226f07b74ed40709f60c72e8
karkraz <rochadcmarcos@gmail.com>
2023-07-12 23:56:07 -0300

    documented ranking.py

--- ranking.py
+++ ranking.py
@@ -8,7 +8,7 @@ inter = InternationalCup()
 
 class Ranking:
     @staticmethod
-    def define_conmebol_points(clubs):
+    def define_conmebol_points(clubs) -> None:
         ''' Define the commebol ranking based on the csv file '''
         
         with open('files/conmebol/ranking_conmebol.csv', encoding='utf8') as file:
@@ -26,7 +26,7 @@ class Ranking:
                         club.ranking_points += val    
 
     @staticmethod
-    def individual_leaderboard(all_clubs, category, season, save_file=None):
+    def individual_leaderboard(all_clubs, category, season, save_file=None) -> pd.Series:
         """ Return a dataframe contains name, current_club, position, matches_played, goals, assists, average """
         
         series_data = []
@@ -51,7 +51,7 @@ class Ranking:
         return sorted_data_frame[:10]
 
     @staticmethod 
-    def domestic_table(division, season):
+    def domestic_table(division, season) -> pd.DataFrame:
         ''' 
         Return a panda series with the tables below
         Position    Club    Matches    Won  Draw    Lost   Goals For    Goals Away    Goals Diff    Points
@@ -63,17 +63,16 @@ class Ranking:
         return df
 
     @staticmethod
-    def get_domestic_champion(division, season):
-        ''' Return the champions '''
+    def get_domestic_champion(division, season) -> pd.Series:
+        ''' Return the domestic champion '''
         value = league.get_domestic_cup_table(division, season)
         
         df = pd.DataFrame(value, index=None, columns=['id','Club','Matches','Won','Draw','Lost','GF','GA','GD','Points'])
         
-        # return df.iloc[0]
         return df.iloc[:1]["Club"]
 
     @staticmethod
-    def international_group_table(season):
+    def international_group_table(season) -> list:
         ''' 
         Return a panda series with the group stage tables
         '''
@@ -91,7 +90,7 @@ class Ranking:
         return dfs
     
     @staticmethod
-    def player_info(players):
+    def player_info(players) -> pd.DataFrame:
         '''
         Return a squad dataframe from the players inside the club
         '''
