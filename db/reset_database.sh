bc47d3738cc9502e7af5fe21ed1f84d578ed9704
karkraz <rochadcmarcos@gmail.com>
2023-07-21 21:26:13 -0300

    remove set international through sh

--- db/reset_database.sh
+++ db/reset_database.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #!/bin/sh
     
-echo "Reseting database"
 rm -r db/database.db 
-echo "Recreating Base Database"
 python db/database.py $1
-echo "Recretating International Database"
-python setting_international_clubs.py 
