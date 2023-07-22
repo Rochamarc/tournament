b68a992cdd40c94ae8289a6ab5ab980ecad58a0f
karkraz <rochadcmarcos@gmail.com>
2023-07-22 01:13:09 -0300

    style update/remove print from players controller

--- db/player_controller.py
+++ db/player_controller.py
@@ -9,7 +9,7 @@ class PlayerData():
     @staticmethod
     def get_players(club_name) -> list:
         ''' 
-        Get the players info from database
+        Get the players data from database by club name
         '''
 
         conn = sqlite3.connect(database)
@@ -23,68 +23,55 @@ class PlayerData():
         return data
 
     @staticmethod
-    def insert_players_db(players, verbose=False) -> bool:
+    def insert_players_db(players) -> None:
         '''
         Insert players data into the database
         '''
-
-        if verbose: print("Inserting players on the database")
         
         conn = sqlite3.connect(database)
         cursor = conn.cursor()        
 
-        
         for player in players:        
-            if verbose : print(f"Insert player {player} into the database")
             player_data = player.data()
             cursor.execute(qh.open_insert_query('players'), player_data)
         
         conn.commit()
         conn.close()
 
-        if verbose : print("Players inserted into the database sucessfully!")
-
-        return True
+        return None
 
     @staticmethod
-    def update_player_stats(player_list, verbose=False):
+    def update_player_stats(player_list) -> None:
         ''' Update players database '''
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
         
         for player in player_list:
-            # print('.', sep=' ', end=' ', flush=True)
-            
             player_data = player.get_competition_stats()
-
-            if verbose : print(f"Update {player}")
-
             cursor.execute(qh.open_update_query('players'), player_data)
 
         conn.commit()
         conn.close()
 
-        if verbose : print("Player updated sucessfully!")
-        return True
+        return None
 
     @staticmethod 
-    def update_players_age(player_list, verbose=False):
-        ''' Update players age '''
-
+    def update_players_age(player_list) -> None:
+        ''' Update players age row from database '''
+        
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
 
         for player in player_list:
-            if verbose: print(f"Update: {player}")
-
             cursor.execute(qh.open_update_query('players_age'), [1, player.id])
 
         conn.commit()
         conn.close()
-        return True
+        return None
 
     @staticmethod
-    def update_players_retirement(player_list: list) -> bool:
+    def update_players_retirement(player_list: list) -> None:
+        ''' Update players retirement row from database '''
         
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
@@ -94,7 +81,7 @@ class PlayerData():
 
         conn.commit()
         conn.close()
-        pass 
+        return None
     
     @staticmethod
     def get_all_players() -> list:
