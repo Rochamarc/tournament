57a771b45b73ecc1a514294f3eb0a1ae3559f5f6
karkraz <rochadcmarcos@gmail.com>
2023-08-22 14:24:39 -0300

    reconstuct clubs

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from classes.player import Player
+from classes.club import Club
 
 class ClassConstructor:
 	@staticmethod
@@ -9,3 +10,11 @@ class ClassConstructor:
 			players.append(Player(pd[0], pd[1], pd[2], pd[3], pd[4], pd[5], pd[6], pd[7], pd[8]))
 
 		return players
+
+	@staticmethod
+	def reconstruct_clubs(clubs_data: list) -> list[Club]:
+		clubs = []
+		for cd in clubs_data:
+			clubs.append(Club(cd[0], cd[1], cd[2], cd[3], cd[4], cd[5]))
+
+		return clubs 
