bf530210687efe0564280990ba8ef5125a999605
karkraz <rochadcmarcos@gmail.com>
2023-08-22 14:24:06 -0300

    add id to club class

--- classes/club.py
+++ classes/club.py
@@ -1,6 +1,6 @@
 class Club:
-    def __init__(self,name: str,country: str, club_class: str, total_budget: int, salary_budget: int):
-        self.id = None
+    def __init__(self,id: int, name: str,country: str, club_class: str, total_budget: int, salary_budget: int):
+        self.id = id
         self.name = name
         self.country = country
         self.short_country = self.country[:3].upper()
