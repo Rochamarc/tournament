9a4f843e23384a52cf35f03a1e2b319dc173ff4c
karkraz <rochadcmarcos@gmail.com>
2023-09-22 00:29:25 -0300

    change clubs and base controller

---
+++ db/base_controller.py
@@ -0,0 +1,19 @@
+class BaseController:
+    @classmethod
+    def get_select_query(cls, file_name: str) -> str:
+        ''' Return a string query by a file in sql '''
+        try:
+            with open('db/queries/select/{}.sql'.format(file_name), 'r') as file:
+                return ''.join(file.readlines())
+        except: 
+            raise FileNotFoundError("File name doesn't exists")
+    
+    @classmethod
+    @property
+    def database_config(cls):
+        return { 
+            'user': 'tournament_user', 
+            'host': 'localhost', 
+            'password': 'tournament_pass', 
+            'database': 'tournament' 
+        }
