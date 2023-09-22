0dfc2e21191cee86965a291a4f67c60fc393eb28
karkraz <rochadcmarcos@gmail.com>
2023-09-22 00:15:23 -0300

    add select serie c clubs

---
+++ db/queries/select/select_serie_c_clubs.sql
@@ -0,0 +1,7 @@
+SELECT clubs.id, clubs.name, clubs.country, divisons.name  
+FROM clubs 
+INNER JOIN championships 
+INNER JOIN divisons 
+    ON divison_id=divisons.id 
+    AND clubs.id = championships.club_id
+    AND divisons.name = 'Serie C';
