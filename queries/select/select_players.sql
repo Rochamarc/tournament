01eecda7d882313d078bf3f7fe7a2a8c8a623edb
karkraz <rochadcmarcos@gmail.com>
2023-09-24 21:52:54 -0300

    delete queries repository

--- queries/select/select_players.sql
+++
@@ -1,5 +0,0 @@
-SELECT players.id, players.name, players.nationality, players.birth_year, players.position, 
-players.height, players.weight, players.foot, players_overall.overall
-FROM players
-INNER JOIN players_overall
-	ON players.id = players_overall.id_player;
