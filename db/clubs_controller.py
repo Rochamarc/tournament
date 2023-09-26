b514e3ac70714b59a29e9badda501ebfd4402c4b
karkraz <rochadcmarcos@gmail.com>
2023-09-26 14:37:05 -0300

    clubs construction

--- db/clubs_controller.py
+++ db/clubs_controller.py
@@ -1,10 +1,10 @@
 import mysql.connector
-from base_controller import BaseController
+from db.base_controller import BaseController
 
 class ClubsController(BaseController):
     @classmethod
     def select_serie_a_clubs(cls) -> list[set]:
-        ''' Returns a list of players based on divison 
+        ''' Returns a list of clubs based on divison 
 
             warning: this will receive an argument season that have 
             a clause inside the query
@@ -20,7 +20,7 @@ class ClubsController(BaseController):
 
     @classmethod
     def select_serie_b_clubs(cls) -> list[set]:
-        ''' Returns a list of players based on divison 
+        ''' Returns a list of clubs based on divison 
 
             warning: this will receive an argument season that have 
             a clause inside the query
@@ -37,7 +37,7 @@ class ClubsController(BaseController):
 
     @classmethod
     def select_serie_c_clubs(cls) -> list[set]:
-        ''' Returns a list of players based on divison 
+        ''' Returns a list of clubs based on divison 
 
             warning: this will receive an argument season that have 
             a clause inside the query
