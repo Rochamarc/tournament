a82b964c3ab7657c993c404d33ca29811636da2e
karkraz <rochadcmarcos@gmail.com>
2023-09-29 19:24:35 -0300

    new methods to clubs

--- classes/club.py
+++ classes/club.py
@@ -1,3 +1,7 @@
+# start_eleven -> will have to be another type of data storage because it only accept 11
+#
+#
+
 class Club:
     def __init__(self, id: int, name: str, country: str):
         self.id = id
@@ -14,3 +18,9 @@ class Club:
     
     def add_to_squad(self, *values: list):
         self.squad += values
+
+    def add_to_start_eleven(self, *values: list):
+        self.start_eleven += values 
+    
+    def add_to_bench(self, *values: list):
+        self.bench += values
