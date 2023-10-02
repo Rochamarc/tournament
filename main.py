85c0eea08b401e3f9436b3da7bb9f3de204039df
karkraz <rochadcmarcos@gmail.com>
2023-10-01 21:44:00 -0300

    add main.py not tested

--- main.py
+++ main.py
@@ -11,11 +11,35 @@ formation = Formation()
 p = players_controller.select_players_with_contract('2022') # get from db
 players = cc.players(p) # transform into objects
 
+# serie a
 serie_a = clubs_controller.select_serie_a_clubs()
-clubs = cc.clubs(serie_a)
-clubs = cc.add_players_to_clubs(clubs, players)
+serie_a_clubs = cc.clubs(serie_a)
+serie_a_clubs = cc.add_players_to_clubs(serie_a_clubs, players)
 
-for club in clubs:
+# serie b
+serie_b = clubs_controller.select_serie_b_clubs()
+serie_b_clubs = cc.clubs(serie_b)
+serie_b_clubs = cc.add_players_to_clubs(serie_b_clubs, players)
+
+# serie c
+serie_c = clubs_controller.select_serie_c_clubs()
+serie_c_clubs = cc.clubs(serie_c)
+serie_c_clubs = cc.add_players_to_clubs(serie_c_clubs, players)
+
+
+for club in serie_a_clubs:
+    ''' Define the start eleven and the bench '''
+    club.start_eleven = formation.starting_eleven(club.squad)
+    club.bench = formation.backups(club.squad, club.start_eleven)
+
+
+for club in serie_b_clubs:
+    ''' Define the start eleven and the bench '''
+    club.start_eleven = formation.starting_eleven(club.squad)
+    club.bench = formation.backups(club.squad, club.start_eleven)
+
+
+for club in serie_c_clubs:
     ''' Define the start eleven and the bench '''
     club.start_eleven = formation.starting_eleven(club.squad)
     club.bench = formation.backups(club.squad, club.start_eleven)
