f636d62a176a372b5c83263731545274e2b5640c
karkraz <rochadcmarcos@gmail.com>
2023-10-10 18:29:17 -0300

    main with games initialize

--- main.py
+++ main.py
@@ -2,58 +2,49 @@ from helper import ClassConstructor
 from db.players_controller import PlayersController
 from db.clubs_controller import ClubsController
 from db.stadiums_controller import StadiumsController
-from classes.formation import Formation
-from game import Game 
-
-def define_formation(clubs: list) -> None:
-    for club in clubs:
-        club.start_eleven = formation.starting_eleven(club.squad)
-        club.bench = formation.backups(club.squad, club.start_eleven)
-
-def define_schedule(clubs_list: list):
-    games = []
-    for home in clubs_list:
-        for away in clubs_list:
-            if home != away : games.append([home, away]) 
-    return games
-
-def prepare_games(games: list, competition: str, season: int) -> list[Game]:
-    pass 
 
 
+# Static classes
 class_const = ClassConstructor()
 players_controller = PlayersController()
 clubs_controller = ClubsController()
 stadiums_controller = StadiumsController()
-formation = Formation()
 
+
+# Select and transform stadium data into objects
+stadiums_data = stadiums_controller.select_all_stadiums()
+stadiums = class_const.stadiums(stadiums_data)
+
+# Select players with contract
 p = players_controller.select_players_with_contract('2022') # get from db
 players = class_const.players(p) # transform into objects
 
-# serie a clubs
+# Serie A clubs selection & cast
 serie_a = clubs_controller.select_serie_a_clubs()
 serie_a_clubs = class_const.clubs(serie_a)
 serie_a_clubs = class_const.add_players_to_clubs(serie_a_clubs, players)
 
-# serie b clubs
+# Serie B clubs selection & cast
 serie_b = clubs_controller.select_serie_b_clubs()
 serie_b_clubs = class_const.clubs(serie_b)
 serie_b_clubs = class_const.add_players_to_clubs(serie_b_clubs, players)
 
-# serie c clubs
+# Serie C clubs selection & cast
 serie_c = clubs_controller.select_serie_c_clubs()
 serie_c_clubs = class_const.clubs(serie_c)
 serie_c_clubs = class_const.add_players_to_clubs(serie_c_clubs, players)
 
-# this will set the clubs start eleven and bench
-define_formation(serie_a_clubs)
-define_formation(serie_b_clubs)
-define_formation(serie_c_clubs)
+# Set clubs formation
+class_const.define_formation(serie_a_clubs)
+class_const.define_formation(serie_b_clubs)
+class_const.define_formation(serie_c_clubs)
 
-serie_a_schedule = define_schedule(serie_a_clubs)
-serie_b_schedule = define_schedule(serie_b_clubs)
-serie_c_schedule = define_schedule(serie_c_clubs)
+# Define the clubs matches, this will return a list of lists with two differents teams
+serie_a_schedule = class_const.define_schedule(serie_a_clubs)
+serie_b_schedule = class_const.define_schedule(serie_b_clubs)
+serie_c_schedule = class_const.define_schedule(serie_c_clubs)
 
-stadiums_data = stadiums_controller.select_all_stadiums()
-stadiums = class_const.stadiums(stadiums_data)
-
+# Return a list of games object with all the confrontations through the season
+serie_a_games = class_const.prepare_games(serie_a_schedule, stadiums, 'Campeonato Brasileiro Serie A', 2022)
+serie_b_games = class_const.prepare_games(serie_b_schedule, stadiums, 'Campeonato Brasileiro Serie B', 2022)
+serie_c_games = class_const.prepare_games(serie_c_schedule, stadiums, 'Campeonato Brasileiro Serie C', 2022)
