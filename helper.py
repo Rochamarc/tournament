612a789bb68818188f2500fbcf6a00a8e0b24934
karkraz <rochadcmarcos@gmail.com>
2023-10-10 18:28:55 -0300

    update helper with the extra functions in main.py

--- helper.py
+++ helper.py
@@ -1,6 +1,13 @@
-from classes.player import Player
 from classes.club import Club
+from classes.formation import Formation
+from classes.player import Player
 from classes.stadium import Stadium
+
+from game_test import Game 
+
+from random import choice 
+
+formation = Formation()
 
 class ClassConstructor:
 	@staticmethod
@@ -38,3 +45,28 @@ class ClassConstructor:
 				if player.club_id == club.id: 
 					club.add_to_squad(player)
 		return clubs 
+	
+	@staticmethod
+	def define_formation(clubs: list) -> None:
+		''' Return None, just change the players situation inside the Club objects '''
+		
+		for club in clubs:
+			club.start_eleven = formation.starting_eleven(club.squad)
+			club.bench = formation.backups(club.squad, club.start_eleven)
+	
+		return None 
+	
+	@staticmethod
+	def define_schedule(clubs_list: list):
+		''' Return a list of lists wich one with home_team and away_team '''
+
+		games = []
+		for home in clubs_list:
+			for away in clubs_list:
+				if home != away : games.append([home, away]) 
+		return games
+
+	@staticmethod
+	def prepare_games(games: list, stadiums: list, competition: str, season: int) -> list[Game]:
+		''' Return a list of Game objetcs '''
+		return [ Game(i[0], i[1], competition, 1, season, choice(stadiums)) for i in games ]
