29be691564b62fd84b91a1e1fa3cd0098c5bd5b9
karkraz <rochadcmarcos@gmail.com>
2023-10-14 15:46:51 -0300

    add BaseController.get_insert_query

--- db/base_controller.py
+++ db/base_controller.py
@@ -18,6 +18,15 @@ class BaseController:
             raise FileNotFoundError("File name doesn't exists")
 
     @classmethod
+    def get_insert_query(cls, file_name: str) -> str:
+        ''' Return a string query by a file in sql '''
+        try:
+            with open('db/queries/insert/{}.sql'.format(file_name), 'r') as file:
+                return ''.join(file.readlines())
+        except:
+            raise FileNotFoundError("File name doesn't exists")
+        
+    @classmethod
     @property
     def database_config(cls):
         return { 
