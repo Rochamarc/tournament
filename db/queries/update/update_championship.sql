9945726e2fb676067b69daee15dae508429b67b7
karkraz <rochadcmarcos@gmail.com>
2023-10-27 23:46:09 -0300

    fix db queries and controllers errors & bugs

--- db/queries/update/update_championship.sql
+++ db/queries/update/update_championship.sql
@@ -1,3 +1,10 @@
 UPDATE championships 
-SET matches = %s, won = %s, draw = %s, lost = %s, goals_for = %s, goals_away = %s, goall_diff = %s
-WHERE (championships.club_id = %s AND championships.season = %s);
+SET 	matches = matches + 1, 
+		win = %s, 
+		draw = %s, 
+		loss = %s, 
+		goals_for = %s, 
+		goals_away = %s, 
+		goals_diff = %s
+WHERE 	(championships.club_id = %s 
+	AND championships.season = %s);
