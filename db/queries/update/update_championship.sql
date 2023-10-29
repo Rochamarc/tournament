48f08c77b8f0ffa80c28fb48f051d8da6000b26e
karkraz <rochadcmarcos@gmail.com>
2023-10-29 13:40:33 -0300

    change update_championships sql

--- db/queries/update/update_championship.sql
+++ db/queries/update/update_championship.sql
@@ -1,10 +1,10 @@
 UPDATE championships 
 SET 	matches = matches + 1, 
-		win = %s, 
-		draw = %s, 
-		loss = %s, 
-		goals_for = %s, 
-		goals_away = %s, 
-		goals_diff = %s
+		win = win + %s, 
+		draw = draw + %s, 
+		loss = loss + %s, 
+		goals_for = goals_for + %s, 
+		goals_away = goals_away + %s, 
+		goals_diff = goals_diff + %s
 WHERE 	(championships.club_id = %s 
 	AND championships.season = %s);
