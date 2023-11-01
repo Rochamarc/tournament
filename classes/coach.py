8c3362ee4930f074c9005d59a7a7409b92380b22
karkraz <rochadcmarcos@gmail.com>
2023-11-01 17:53:05 -0300

    Add docstrings to classes files

--- classes/coach.py
+++ classes/coach.py
@@ -1,6 +1,23 @@
 from classes.person import Person
 
 class Coach(Person):
+    """
+    A class that represents a Football/Soccer Coach
+
+    ...
+
+    Attributes
+    ----------
+        id :  int
+            Unique value the representes each coach
+        name : str
+            The name of the coach
+        nationality : str
+            The nationality from the coach
+        birth : int
+            The birth year of the coach
+    """
+
     def __init__(self, id: int, name: str, nationality: str, birth: int):
         super().__init__(name, nationality, birth)
         self.id = id 
--- classes/player.py
+++ classes/player.py
@@ -1,6 +1,35 @@
 from classes.person import Person 
 
 class Player(Person):
+    """
+    A class that represents a Football/Soccer player
+
+    ...
+
+    Attributes
+    ----------
+        id :  int
+            Unique value the representes each coach
+        name : str
+            The name of the coach
+        nationality : str
+            The nationality from the coach
+        birth : int
+            The birth year of the coach
+        position : str
+            Two capital letters that represents the players position
+        height : float
+            Players height
+        weight : float
+            Players weight
+        foot : str
+            A capital letter that represents the player favorite foot
+        overall : int
+            A value from 56 to 100 that represents how good the player is
+        club_id : int
+            The id from the club that the athlete plays for 
+    """
+
     def __init__(self, id: int, name: str, nationality: str, birth: int, position: str, height: float, weight: float, foot: str, overall: int, club_id: int):
         super().__init__(name, nationality, birth)
         self.id = id
