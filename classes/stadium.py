34d2367e99b83c60a05b220a61864a1b8842f39a
karkraz <rochadcmarcos@gmail.com>
2023-11-01 17:53:05 -0300

    Add docstrings to classes files

--- classes/stadium.py
+++ classes/stadium.py
@@ -1,5 +1,20 @@
 class Stadium:
-    def __init__(self, name, location, capacity):
+    """
+    A class that represents a Football/Soccer Stadium
+
+    ...
+
+    Attributes
+    ----------
+        name : str
+            The name of the Stadium
+        location : str
+            The location of the stadium
+        capacity : int
+            The maximum capacity of audience supported by the stadium
+    """
+
+    def __init__(self, name: str, location: str, capacity: int):
         self.name = name
         self.location = location
         self.capacity = capacity
