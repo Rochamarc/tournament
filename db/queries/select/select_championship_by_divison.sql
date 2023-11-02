2c0977779bce4b431cdf25bc6a2bdc92b043b3e7
karkraz <rochadcmarcos@gmail.com>
2023-11-02 00:34:55 -0300

    update select championships by division

--- db/queries/select/select_championship_by_divison.sql
+++ db/queries/select/select_championship_by_divison.sql
@@ -2,4 +2,5 @@ SELECT clubs.name matches, won, draw, lost, goals_for, goals_away, goall_diff, p
 FROM championships 
 INNER JOIN clubs
     ON (clubs.id = championships.club_id)
-    AND (championships.season = %s AND championships.division = %s);
+WHERE championships.season = %s AND championships.division = %s
+ORDER BY championships.points DESC;
