26820c08b57a6bced4ad046ba44d5cbe3c6471b1
karkraz <rochadcmarcos@gmail.com>
2023-11-02 00:31:25 -0300

    update championships controller & add docstings

--- db/championship_controller.py
+++ db/championship_controller.py
@@ -2,9 +2,36 @@ import mysql.connector
 from db.base_controller import BaseController
 
 class ChampionshipsController(BaseController):
+    """
+    Class that manage the tournament.championships table
+
+    ...
+
+    Methods
+    -------
+
+    select_championships_table_by_club(season: int, club_id: int)
+        Select championships table by club
+    
+    """
+    
     @classmethod
     def select_championship_table_by_club(cls, season: int, club_id: int) -> list[set]:
-        ''' Select championship club table by club '''
+        """Select the club's championships table by his id
+
+        Parameters
+        ----------
+        season : int
+            To select the season of the championships
+        club_id : int
+            To select the id from the club's championships
+
+        Returns
+        -------
+            A list containing 
+            matches, win, draw, loss, goals_for, goals_away, goals_conceded, goals_diff, points
+        """
+
         conn = mysql.connector.connect(**cls.database_config)
         cursor = conn.cursor()
 
@@ -16,7 +43,21 @@ class ChampionshipsController(BaseController):
     
     @classmethod
     def select_championship_table_by_division(cls, season: int, division_name: str) -> list[set]:
-        ''' Select championship club table by club '''
+        """Select all the rows of the championsips table based on the season and divison
+
+        Parameters
+        ----------
+        season : int
+            To select the season of the championships
+        division_name : str
+            To select the championships division
+        
+        Returns
+        -------
+            A list of lists containing 
+            matches, win, draw, loss, goals_for, goals_away, goals_diff, points
+        """
+                
         conn = mysql.connector.connect(**cls.database_config)
         cursor = conn.cursor()
 
@@ -27,19 +68,23 @@ class ChampionshipsController(BaseController):
         return values
 
     @classmethod
-    def update_championship_table(cls, season: str, club_id: int, update_data: list) -> None:
-        ''' Update championships table in database
-            season: the season of the table to be updated
-            club_id: club that will have the table updated
-            update_data: [ matches, won, draw, lost, goals_for, goals_away, goall_diff, points ] 
-                list of values to be updated in the list
-        '''
+    def update_championship_table(cls, data: list) -> None:
+        """Update the club's championships table based on division
+        
+        Parameters
+        ----------
+        data : list
+            A list containing: points, win, loss, draw, goals_for, goals_away, goal_diff, club_id, season
+        
+        Returns
+        -------
+            None
+        """
+
         conn = mysql.connector.connect(**cls.database_config)
         cursor = conn.cursor()
 
-        update_data += [club_id, season]
-
-        cursor.execute(cls.get_update_query('update_championship'), update_data)
+        cursor.execute(cls.get_update_query('update_championship'), data)
         
         conn.commit()
         conn.close()
