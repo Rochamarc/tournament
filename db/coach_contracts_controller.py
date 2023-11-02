cb5d354ccfbaf37bf55ef2727148c4a97fdcbd8d
karkraz <rochadcmarcos@gmail.com>
2023-11-02 16:06:58 -0300

    refact tournament.config module -> coach part
    Refact the coaches & coach_contracts. Adding new controllers, new methods to existing controllers and new queries (not tested yet)

---
+++ db/coach_contracts_controller.py
@@ -0,0 +1,41 @@
+import mysql.connector
+from db.base_controller import BaseController
+
+class CoachContractsController(BaseController):
+    """
+    Class that manage the tournament.coach_contracts
+
+    ...
+
+    Methods
+    -------
+    insert_coach_contracts(coach_contracts_data: list)
+        Insert a list of tournament.coach_contracts in database
+    """
+
+    @classmethod
+    def insert_player_contracts(cls, coach_contracts_data: list) -> None:
+        """Insert a list of player_contracts_data into tournament.coach_contracts
+
+        Parameters
+        ----------
+        coach_contracts_data : list
+            A list of lists containing: 
+            start: str, end: str, salary: int, club_id: int, coach_id: int
+        
+        Returns
+        -------
+            None
+        """
+        
+        conn = mysql.connector.connect(**cls.database_config)
+        cursor = conn.cursor()
+
+        for coach_contract in coach_contracts_data:
+            cursor.execute(cls.get_insert_query('insert_coach_contracts'), coach_contract)
+        
+        conn.commit()
+        conn.close()
+
+        return None 
+    
---
+++ db/queries/insert/insert_coach_contracts.sql
@@ -0,0 +1,1 @@
+INSERT INTO coach_contracts (start, end, salary, club_id, coach_id) VALUES(%s, %s, %s, %s, %s);
