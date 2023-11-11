bd69f3974511961fa464f7a63c3fb5868b40aca4
karkraz <rochadcmarcos@gmail.com>
2023-11-11 03:10:44 -0300

    feat: add get_delete_query method

--- db/base_controller.py
+++ db/base_controller.py
@@ -2,7 +2,34 @@ class BaseController:
     """
     Base class that contains every property and functions used in other controllers
     """
-    
+
+    @classmethod
+    def get_delete_query(cls, file_name: str) -> str:
+        """Get one query on db/queries/select
+
+        Parameters
+        ----------
+        file_name : str
+            Name of the file inside the path without the file extension
+        
+        Returns
+        -------
+            A string with the query in the file
+
+        Raises
+        ------
+            FileNotFoundError
+                If the file doesnt exists
+        """
+        
+        file_path = 'db/queries/delete/{}.sql'.format(file_name)
+        try:
+            with open(file_path, 'r') as file:
+                return ''.join(file.readlines())
+        except: 
+            raise FileNotFoundError("File {} doesn't exists".format(file_path))
+
+
     @classmethod
     def get_select_query(cls, file_name: str) -> str:
         """Get one query on db/queries/select
