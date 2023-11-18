4a36d1f1570f4c042854cbfa943644e61121afc2
karkraz <rochadcmarcos@gmail.com>
2023-11-18 02:06:55 -0300

    fix: update select_championships_by_division query

--- db/queries/select/select_championship_by_divison.sql
+++ db/queries/select/select_championship_by_divison.sql
@@ -1,6 +1,16 @@
-SELECT clubs.name matches, won, draw, lost, goals_for, goals_away, goall_diff, points
+SELECT  clubs.name, 
+        championships.matches, 
+        championships.win, 
+        championships.draw, 
+        championships.loss, 
+        championships.goals_for, 
+        championships.goals_away, 
+        championships.goals_diff, 
+        championships.points
 FROM championships 
 INNER JOIN clubs
     ON (clubs.id = championships.club_id)
-WHERE championships.season = %s AND championships.division = %s
+INNER JOIN divisions
+    ON (divisions.id = championships.division_id)
+WHERE (championships.season = %s AND divisions.name = %s)
 ORDER BY championships.points DESC;
