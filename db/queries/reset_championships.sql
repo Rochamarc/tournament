d009c5cbc477c744db7b686ec8b202e14702abc9
karkraz <rochadcmarcos@gmail.com>
2023-11-19 02:54:45 -0300

    feat: reset_championships query

--- db/queries/reset_championships.sql
+++ db/queries/reset_championships.sql
@@ -1,3 +1,5 @@
+-- Zera a tabela
+
 UPDATE championships SET
     matches = 0,
     win = 0,
@@ -6,5 +8,8 @@ UPDATE championships SET
     goals_for = 0,
     goals_away = 0,
     goals_diff = 0,
-    points = 0,
+    points = 0
 WHERE season = '2022';
+
+-- Remove os clubs da tabela
+DELETE FROM championships WHERE season = '2023';
