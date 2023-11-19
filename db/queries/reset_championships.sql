d7bb07128ff9061444f9f5316aa5e5f26f010ef5
karkraz <rochadcmarcos@gmail.com>
2023-11-19 01:44:55 -0300

    feat: reset_championships query

---
+++ db/queries/reset_championships.sql
@@ -0,0 +1,10 @@
+UPDATE championships SET
+    matches = 0,
+    win = 0,
+    loss = 0,
+    draw = 0,
+    goals_for = 0,
+    goals_away = 0,
+    goals_diff = 0,
+    points = 0,
+WHERE season = '2022';
