357756a275fa957b8ef729cc5a4625166bc7187a
karkraz <rochadcmarcos@gmail.com>
2023-11-26 17:46:49 -0300

    refactor: update cup module to use functions
    it's raising a type error and have to be a CupGame object on game instead of just Game

--- cup.py
+++ cup.py
@@ -2,16 +2,25 @@ from db.championships_controller import ChampionshipsController
 from db.clubs_controller import ClubsController
 from db.games_controller import GamesController
 from db.stadiums_controller import StadiumsController
+from db.players_controller import PlayersController
 
+from logs_helper import LogsHandler
 from data_manipulation import fomrmulate_clubs_to_simple_cup
-from helper import ClassConstructor
+from helper import ClassConstructor, CupHelper
 
 # Declare my controllers
 championships_controller = ChampionshipsController()
-games_controller = GamesController()
-class_constructor = ClassConstructor()
-stadiums_controller = StadiumsController()
 clubs_controller = ClubsController()
+games_controller = GamesController()
+players_controller = PlayersController()
+stadiums_controller = StadiumsController()
+
+logs_handler = LogsHandler()
+
+class_constructor = ClassConstructor()
+cup_helper = CupHelper()
+
+initial_season = '2022'
 
 
 # Select stadiums 
@@ -19,22 +28,30 @@ stadiums_data = stadiums_controller.select_all_stadiums()
 stadiums = class_constructor.stadiums(stadiums_data)
 
 # Select clubs from serie a
-a = championships_controller.select_serie_a_cup('2022')
+a = championships_controller.select_serie_a_cup(initial_season)
 
 # Select 12 random clubs from serie b or c
-bc = championships_controller.select_serie_b_c_cup('2022')
+bc = championships_controller.select_serie_b_c_cup(initial_season)
 
-# Formulate list with ids
+# Formulate list with ids of clubs that will player the cup
 ids = fomrmulate_clubs_to_simple_cup(a,bc)
 
-# Get clubs data
-clubs_data = [ clubs_controller.select_club_by_id(i)[0] for i in ids ]
+# The next 8 lines are all about clubs & players config before they can play the game
+clubs_data = [ clubs_controller.select_club_by_id(i)[0] for i in ids ] # Get clubs data
+players_data = players_controller.select_players_with_contract(initial_season) # Get players from database
 
-# Get clubs objects
-clubs = clubs_controller.clubs(clubs_data)
+clubs = class_constructor.prepare_clubs(clubs_data, players_data)
 
-# Define the condronts
-game_data = class_constructor.sort_simple_cup_confronts(clubs)
+# First leg of round of 32
+game_data = cup_helper.sort_simple_cup_confronts(clubs) # Define the condronts
+games = class_constructor.prepare_games(game_data, stadiums, 'Copa do Brasil', int(initial_season)) # Here are the round of 32 games
 
-# Prepare games
-class_constructor.prepare_games(game_data, stadiums, 'Copa do Brasil', 2022)
+rnd_32 = cup_helper.run_cup_phase('Round of 32', 1, 3, games)
+print(rnd_32)
+
+# Second leg
+game_data = cup_helper.invert_confronts(game_data)
+games = class_constructor.prepare_games(game_data, stadiums, 'Copa do Brasil', int(initial_season))
+
+rnd_32 = cup_helper.run_cup_phase('Round of 32', 2, 3, games)
+print(rnd_32)
