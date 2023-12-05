18676c2c3af38934ec978ea5601433f892a5e470
karkraz <rochadcmarcos@gmail.com>
2023-12-05 01:25:00 -0300

    feat: update knock_out queries

--- db/queries/insert/insert_knock_out_first_leg.sql
+++ db/queries/insert/insert_knock_out_first_leg.sql
@@ -1,10 +1,5 @@
-INSERT INTO knock_out(  season, 
-                        phase, 
+INSERT INTO knock_out(  phase, 
                         single_match, 
                         match_number, 
-                        home_id, 
-                        away_id, 
-                        home_game_stats_id, 
-                        away_game_stats_id, 
-                        competition_id ) 
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+                        game_id) 
+VALUES (%s, %s, %s, %s);
