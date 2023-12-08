ae7c5498c0685a15085761414064bd5807cb2fff
karkraz <rochadcmarcos@gmail.com>
2023-12-07 21:49:43 -0300

    feat: remove init from NameGenerator

--- NameGenerator/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
-from NameGenerator.base_controller import BaseController
-from NameGenerator.names_controller import NamesController
