362ec1cf50074e6ed7ab703c0f10189eb12ec4f9
karkraz <rochadcmarcos@gmail.com>
2023-12-19 11:37:29 -0300

    feat: add more queries to reset_championships sql file

--- db/queries/reset_championships.sql
+++ db/queries/reset_championships.sql
@@ -1,5 +1,16 @@
--- Zera a tabela
+-- Remove os clubs da tabela
+DELETE FROM championships WHERE season = '2023';
 
+/* DELETING FROM SCRATCH */
+DELETE FROM stats;
+DELETE FROM games;
+DELETE FROM game_stats;
+DELETE FROM champions;
+
+-- Here we stablish the initial table without droping the database and recreating
+DELETE FROM championships WHERE season != '2022';
+
+-- Makes the original tables zeros
 UPDATE championships SET
     matches = 0,
     win = 0,
@@ -10,6 +21,3 @@ UPDATE championships SET
     goals_diff = 0,
     points = 0
 WHERE season = '2022';
-
--- Remove os clubs da tabela
-DELETE FROM championships WHERE season = '2023';
