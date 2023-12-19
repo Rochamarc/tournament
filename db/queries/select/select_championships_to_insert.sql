a2dd83a74e3fe14ff81fa08098f9a37a721d2bc6
karkraz <rochadcmarcos@gmail.com>
2023-12-19 11:36:53 -0300

    fix: select championships table bug
    add new parameters to sql query

--- db/queries/select/select_championships_to_insert.sql
+++ db/queries/select/select_championships_to_insert.sql
@@ -3,4 +3,5 @@ SELECT  club_id,
 FROM championships
 INNER JOIN divisions
 ON divisions.id = championships.division_id AND divisions.name = %s
+WHERE championships.season = %s
 ORDER BY championships.points DESC;
