d43d294c56b6b084ec7c7c6fc03f4ab393b411c8
karkraz <rochadcmarcos@gmail.com>
2023-12-29 01:17:42 -0300

    feat: create game_stats on tournament_analysis

--- db/analysis_database/main.sql
+++ db/analysis_database/main.sql
@@ -48,6 +48,43 @@ CREATE TABLE championship_stats(
     points INT
 );
 
+CREATE TABLE game_stats(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    season CHAR(4) NOT NULL,
+    competition VARCHAR(100) NOT NULL,
+    confront VARCHAR(200) NOT NULL,
+    home_club VARCHAR(100) NOT NULL, 
+    home_goals INT NOT NULL,
+    home_shots INT NOT NULL,
+    home_shots_on_target INT NOT NULL,
+    home_fouls INT NOT NULL,
+    home_passes INT NOT NULL,
+    home_wrong_passes INT NOT NULL,
+    home_interceptions INT NOT NULL,
+    home_tackles INT NOT NULL,
+    home_stolen_balls INT NOT NULL,
+    home_saves INT NOT NULL,
+    home_ball_possession INT NOT NULL,
+    home_offsides INT NOT NULL,
+    home_freekicks INT NOT NULL,
+    home_penalties INT NOT NULL,
+    away_club VARCHAR(100) NOT NULL,
+    away_goals INT NOT NULL,
+    away_shots INT NOT NULL,
+    away_shots_on_target INT NOT NULL,
+    away_fouls INT NOT NULL,
+    away_passes INT NOT NULL,
+    away_wrong_passes INT NOT NULL,
+    away_interceptions INT NOT NULL,
+    away_tackles INT NOT NULL,
+    away_stolen_balls INT NOT NULL,
+    away_saves INT NOT NULL,
+    away_ball_possession INT NOT NULL,
+    away_offsides INT NOT NULL,
+    away_freekicks INT NOT NULL,
+    away_penalties INT NOT NULL
+);
+
 
 USE tournament; 
 
@@ -125,3 +162,50 @@ INNER JOIN divisions
     ON (divisions.id = championships.division_id)
 INNER JOIN competitions
     ON (competitions.id = divisions.competition_id);
+
+INSERT INTO tournament_analysis.game_stats
+SELECT	NULL,
+        g.season,
+		c.name,
+		CONCAT(home_club.name, ' x ', away_club.name),
+		home_club.name,
+		home_stats.goals,
+        home_stats.shots,
+        home_stats.shots_on_target,
+        home_stats.fouls,
+        home_stats.passes,
+        home_stats.wrong_passes,
+        home_stats.interceptions,
+        home_stats.tackles,
+        home_stats.stolen_balls,
+        home_stats.saves,
+        home_stats.ball_possession,
+        home_stats.offsides,
+        home_stats.freekicks,
+        home_stats.penalties,
+		away_club.name,
+        away_stats.goals,
+        away_stats.shots,
+        away_stats.shots_on_target,
+        away_stats.fouls,
+        away_stats.passes,
+        away_stats.wrong_passes,
+        away_stats.interceptions,
+        away_stats.tackles,
+        away_stats.stolen_balls,
+        away_stats.saves,
+        away_stats.ball_possession,
+        away_stats.offsides,
+        away_stats.freekicks,
+        away_stats.penalties
+FROM games g
+INNER JOIN game_stats home_stats
+	ON home_stats.id = g.home_game_stats_id 
+INNER JOIN game_stats away_stats
+	ON away_stats.id = g.away_game_stats_id
+INNER JOIN clubs home_club
+	ON home_club.id = home_stats.club_id
+INNER JOIN clubs away_club
+	ON away_club.id = away_stats.club_id
+INNER JOIN competitions c
+	ON g.competition_id = c.id;
