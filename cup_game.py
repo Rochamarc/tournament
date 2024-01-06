55fa459dc2e4af9584843015003fa314c05ab8ea
karkraz <rochadcmarcos@gmail.com>
2024-01-05 23:49:20 -0300

    feat: update and add features to cup_game
    updating the leg_goals, fix penalty update & add missing docstrings

--- cup_game.py
+++ cup_game.py
@@ -3,17 +3,11 @@ from classes.player import Player
 from classes.stadium import Stadium
 from game import Game 
 
-from random import choice
-
-# TODO  1- adicionar um sistema de penaltis (x)
-#       2- adicionar penaltis nos logs
-#       3- declarar um vencedor caso vá aos penaltis
-#       4- modificar a base de dados para se adequar ao novo sistema
-#       
-
 
 class CupGame(Game):
-    """
+    """Class that deals with Cup Game Simulation, generates data and makes game decisions automatically
+    inherits from Game and BaseGame
+
     """
     
     def __init__(self, home: Club, away: Club, competition: str, competition_id: int, 
@@ -41,7 +35,8 @@ class CupGame(Game):
 
     
     def start(self) -> dict:
-        """Initialize a match start
+        """Override the Game.start method, adding new rules and 
+        a penalty shootout
 
         Returns
         -------
@@ -49,7 +44,6 @@ class CupGame(Game):
         """
 
         # subscribe the method of game to adapt the new features that only cup have
-
 
         time = 0
         move_info = self.move(self.home, self.away, 'middle')
@@ -62,20 +56,28 @@ class CupGame(Game):
             # make the game have more passes
             if not move_info['keep_ball_possession'] : time += 1
 
+        if self.game_number == 1:
+            # update this on logs to the next game
+            self.logs['cup_info']['first_leg_home_goals'] = self.home_goal
+            self.logs['cup_info']['first_leg_away_goals'] = self.away_goal
+        
         # update logs numbers
         self.update_scoreboard_on_logs()
         self.update_goals_on_logs()
         self.update_winner_on_logs()
 
+        # here are the rules that defines if the game has penalties or not
         if self.game_number == 2:
             winner = self.check_winner()
             
             if not winner:
                 self.penalty_shootout()
             
-            self.update_penalties_on_logs()
-            self.update_winner_by_penalties_on_logs()
-        
+                self.update_penalties_on_logs()
+                self.update_winner_by_penalties_on_logs()
+            
+  
+
         return self.logs 
     
     def check_winner(self) -> bool:
