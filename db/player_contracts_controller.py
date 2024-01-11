a1e6963e1a842d53af3075e8d7eac7f230e013ce
karkraz <rochadcmarcos@gmail.com>
2024-01-11 01:10:51 -0300

    feat: add more player_contracts selection w controllers

--- db/player_contracts_controller.py
+++ db/player_contracts_controller.py
@@ -4,8 +4,6 @@ class PlayerContractsController(BaseController):
     """
     Class that manage the tournament.player_contracts
 
-    ...
-
     Methods
     -------
     insert_player_contracts(player_contracts_data: list)
@@ -28,3 +26,30 @@ class PlayerContractsController(BaseController):
         """
         
         return cls.insert_registers(cls.get_query('insert', 'insert_player_contracts'), player_contracts_data)
+
+    @classmethod
+    def select_players_with_no_contract(cls) -> list[set]:
+        """Select a list of Free Agent players data from tournament.player_contracts
+
+        Returns
+        -------
+            A list of sets with [player_contracts.id, players.id]
+        """
+
+        return cls.select_register(cls.get_query('select','select_players_with_no_contract'))
+
+    @classmethod
+    def select_players_with_end_contract(cls, season: str) -> list[set]:
+        """Select a list of Free Agent players data from tournament.player_contracts
+
+        Parameters
+        ----------
+        season : str
+            An string value with end of season 
+        
+        Returns
+        -------
+            A list of sets with [player_contracts.id, players.id]
+        """
+
+        return cls.select_register(cls.get_query('select', 'select_players_with_end_contract'), season)
---
+++ db/queries/select/select_players_with_end_contracts.sql
@@ -0,0 +1,8 @@
+SELECT 	pc.id AS 'Player Contract id',
+        p.id AS 'Player id'
+FROM player_contracts pc
+LEFT JOIN clubs c 
+	ON pc.club_id = c.id 
+INNER JOIN players p 
+	ON pc.player_id = p.id
+WHERE pc.end = %s;
