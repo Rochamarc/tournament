e60c6fc41bab2148a130df70b170bbffe8cbde92
karkraz <rochadcmarcos@gmail.com>
2024-01-11 01:15:09 -0300

    feat: improve contract generation
    generates contract extension by age

--- data_generator.py
+++ data_generator.py
@@ -1,10 +1,4 @@
-from db.players_controller import PlayersController
-from db.skills_controller import SkillsController
-
 from random import randint
-
-players_controller = PlayersController()
-skills_controller = SkillsController()
 
 def get_skill_by_position(position: str, club_class: str) -> list[int]:
     '''Define a list of values based on player position 
@@ -57,13 +51,12 @@ def get_skill_by_class(club_class: str) -> int:
         return randint(50,60)
         
 
-def generate_players_skills(season: str, players: list, club_class: str) -> None:
+# TODO change the -> None to list[list]
+def generate_players_skills(players: list, club_class: str) -> None:
     """Generate skill data for players
 
     Parameters
-    ----------
-    season : str
-        A string with season value for the skill
+    ---------- 
     players : list
         A list containing player's data with: id and position
     club_class : str
@@ -87,7 +80,78 @@ def generate_players_skills(season: str, players: list, club_class: str) -> None
         player_skills.append(data)
         
     return player_skills
+
+def define_extension_by_age(age: int, season: str) -> str:
+    """Generates a contract extension based on player's age
+
+    Parameters
+    ----------
+    age : int
+        A integer value for player's age
+    season : str
+        A string for initial season
     
+    Returns
+    -------
+        A four digit int value for end season
+    """
+
+    four_to_six = age >= 16 and age <= 22
+    three_to_five = age > 22 and age <= 27
+    two_to_three = age > 27 and age <= 31
+    one_to_two = age > 31
+
+    if four_to_six:
+        extension = randint(4,6)
+    elif three_to_five:
+        extension = randint(3,5)
+    elif two_to_three:
+        extension = randint(2,3)
+    elif one_to_two:
+        extension = randint(1,2)
+    else:
+        raise ValueError(f'{age}')
+
+    return str(extension + int(season))
+
+def generate_player_contracts(players: list, club_id: int, current_season: str) -> list[list]:
+    """Generates contracts data between players and clubs
+
+    Parameters
+    ----------
+    players : list
+        A list of lists contining players data: id & birth
+    club_id : int
+        A integer value for club id
+    current_season : str
+        A string value with the current season. this will be the start value on contract
+
+    Returns
+    -------
+        A list of lists containing player_contracts data
+    """
+
+    # TODO add transfer amount and termination fine as a none default varibles
+    # insert these variable through the query, to do this change the query
+    # to insert all columns from the player_contracts 
+
+    player_contracts = []
+
+    for player in players:
+        player_id = player[0]
+
+        # used to define extension
+        age = int(current_season) - int(player[-1]) 
+
+        start = current_season
+        end = define_extension_by_age(age, current_season)
+        salary = 100_000 
+
+        contract_data = [ start, end, salary, club_id, player_id ]
+
+        player_contracts.append(contract_data)
+   
+    return player_contracts
 
 if __name__ == "__main__":
-    generate_players_skills('2022')
+    pass
