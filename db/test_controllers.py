93ae9d5e4ec9c55ab5743d36ad543c2dd50222e4
karkraz <rochadcmarcos@gmail.com>
2024-01-12 11:40:52 -0300

    test: improving tests with pytest

--- db/test_controllers.py
+++ db/test_controllers.py
@@ -4,16 +4,18 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from db.main_controller import MainController
-from db.player_contracts_controller import PlayerContractsController
 from db.skills_controller import SkillsController
+from db.players_controller import PlayersController
 
-from pprint import pprint
+class TestPlayers:
+    def test_last_players(self):
+        assert len(PlayersController.select_last_players()) == 30
 
-# Running tests on controllers
 
-# print(MainController().select_clubs())
-# print(MainController().select_competitions())
+class TestSkills:
+    def test_skill_in_skills(self):
+        last_skills = (0, 0, 0, 53, 58, 59, 52, 54, 54, 5256)
+        assert last_skills in SkillsController().select_skills('2022')  
 
-# print(PlayerContractsController().select_players_with_no_contract())
-pprint(SkillsController().select_skills('2022'))
+    def test_values_in_skills(self):
+        assert all(SkillsController().select_skills('2022'))
