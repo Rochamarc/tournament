6ed23c0028a597b2f0dc98ddc7a6d497feed377f
karkraz <rochadcmarcos@gmail.com>
2024-01-14 20:12:40 -0300

    refactor: merging controllers to a single controller

--- controllers/coaches_controller.py
+++ controllers/coaches_controller.py
@@ -54,5 +54,21 @@ class CoachesController(BaseController):
 
         return cls.delete_register(cls.get_query('delete', 'delete_coaches'))
 
-if __name__ == "__main__":
-    print(CoachesController().select_id())
+    @classmethod
+    def insert_coach_contracts(cls, coach_contracts_data: list) -> None:
+        """Insert a list of player_contracts_data into tournament.coach_contracts
+
+        Parameters
+        ----------
+        coach_contracts_data : list
+            A list of lists containing: 
+            start: str, end: str, salary: int, club_id: int, coach_id: int
+        
+        Returns
+        -------
+            None
+        """
+
+        return cls.insert_registers(cls.get_query('insert', 'insert_coach_contracts'), coach_contracts_data)
+         
+    
