bbae1c74a42c6169058489fa93b6a5998678af0c
karkraz <rochadcmarcos@gmail.com>
2024-01-14 16:13:11 -0300

    Revert "Revert "refactor: move controllers from db -> controllers""
    This reverts commit 35d760821f5848bdc336b1b6b426fc5878bca56d.

---
+++ controllers/stadiums_controller.py
@@ -0,0 +1,25 @@
+from controllers.base_controller import BaseController
+
+class StadiumsController(BaseController):
+    """
+    Class that manage the tournament.stadiums
+
+    ...
+
+    Methods
+    -------
+
+    select_all_stadiums()
+        Select all stadiums from database
+    """
+        
+    @classmethod
+    def select_all_stadiums(cls) -> list[set]:
+        """Select all stadiums from database
+        
+        Returns
+        -------
+            A list of lists with stadium data
+        """
+
+        return cls.select_register(cls.get_query('select','select_stadiums'))
