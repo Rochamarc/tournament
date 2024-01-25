d2e9adb898031b90e027f54f1c75094f80d1eebf
karkraz <rochadcmarcos@gmail.com>
2024-01-24 23:12:59 -0300

    feat: catching errors on database_initialize

--- database_initialize.sh
+++ database_initialize.sh
@@ -1,5 +1,29 @@
-#!/bin/sh
 #!/bin/bash
+
+#   if [ $exitcode -ne 0 ]; then 
+#       echo "Error on Name Generator databse creation"
+#       exit
+#   else 
+#       echo "Name Generator Database Created Sucessfully"
+#   fi 
+
+function catching_errors(){
+    # $1 exitcode, $2 error_message $3sucess_message
+    exitcode="${1}"
+    error_message="${2}"
+    sucess_message="${3}"
+
+
+    if [ ${exitcode} -ne 0 ]; then
+        # this is the error, and exiting the code
+        echo $error_message
+        exit
+    else
+        # sucess just show the message and pass to next step
+        echo $sucess_message
+    fi
+}
+
 
 # Name generator initialization
 #
@@ -14,18 +38,38 @@ then
     echo "password\t\t: mysql user password"
     exit
 else
+    # NameGenerator initialization
+    #
+    #
+
     echo "Creating NameGenerator database"
     mysql -u "$1" --password="$2" -e "create database tournament_name";
-    echo "Name Generator Database created sucessfully"
+    
+    # return of the code
+    exitcode=$?
+
+    # testing if the code is sucessfull
+    catching_errors $exitcode "Name Generator DB failed" "Name Generator DB Created Sucessfully"
+
+    # Creating tables
+    #
 
     echo "Creating NameGenerator tables"
     mysql -h localhost -u "$1" --password="$2" tournament_name < ./NameGenerator/queries/main.sql
-    echo "Name Generator Tables created sucesfully"
+    
+    exitcode=$?
+
+    catching_errors $exitcode "Name Generator Tables failed" "Name Generator Tables Created sucesfully" 
 
     # populate Name Generator database
+    #
+
     echo "Populating NameGenerator Database"
-    python ./NameGenerator/configure.py
-    echo "NameGenerator Database populated sucessfully"
+    python ./NameGenerator/configure.py 
+
+    exitcode=$?
+
+    catching_errors $exitcode "Name Generator DB Population Failed" "NameGenerator DB Populated Sucessfully"
 
 
     # Tournament initilialization
@@ -33,23 +77,56 @@ else
     #
 
 
+    # Create database
+    #
+
     echo "Creating Tournament database"
     mysql -u "$1" --password="$2" -e "create database tournament";
-    echo "Tournament Database created sucessfully"
+
+    exitcode=$?
+
+    catching_errors $exitcode "Tournament DB failed" "Tournament DB Created Sucessfully"
+
+    # Create tables and constrainsts
+    #
 
     echo "Creating Tournament tables and constraints"
-    mysql -h localhost -u "$1" --password="$2" tournament < ./db/queries/main.sql
-    echo "Tournament tables and constraints created sucessfully"
+    mysql -h localhost -u "$1" --password="$2" tournament < ./db/queries/main.sql || set -e
+    
+    exitcode=$?
+
+    catching_errors $exitcode "Tournament Tables and Constraints Failed" "Tournament Tabled and Constraints Created Sucessfully"
+
+    # Create procedures and triggers
+    #
+
+    echo "Creating procedures and triggers"
+    mysql -h localhost -u "$1" --password="$2" tournament < ./db/queries/procedures_triggers.sql || set -e
+    
+    exitcode=$?
+
+    catching_errors $exitcode "Tournament Procedures and Triggers Failed" "Tournament Procedures and Triggers Created Sucessfully"
+ 
+    # Insert main data on tournament
+    #
 
     echo "Inserting Tournament initial data"
-    mysql -h localhost -u "$1" --password="$2" tournament < ./db/queries/insert/main.sql
-    echo "Tournament data inserted sucessfully"
+    mysql -h localhost -u "$1" --password="$2" tournament < ./db/queries/insert/main.sql || set -e
+
+    exitcode=$?
+
+    catching_errors $exitcode "Tournament Data Insertion Failed" "Tournament Data Inserted Sucessfully"
 
 
     # populate tournament database
+    #
+
     echo "Populating Tournament Database"
-    python ./configure.py
-    echo "Tournament Database populated sucessfully"
+    python ./configure.py || set -e
+
+    exitcode=$?
+
+    catching_errors $exitcode "Tournament Database Population Failed" "Tournament Database Populated Sucessfully"
 fi
 
 
