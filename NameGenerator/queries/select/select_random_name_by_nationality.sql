36d32245af1a7145fe52238851658b61845b6415
karkraz <rochadcmarcos@gmail.com>
2024-01-29 14:52:55 -0300

    refactor: select full name

--- NameGenerator/queries/select/select_random_name_by_nationality.sql
+++ NameGenerator/queries/select/select_random_name_by_nationality.sql
@@ -1,5 +1,1 @@
-SELECT  first_names.value,
-        last_names.value
-FROM first_names, last_names
-WHERE first_names.nationality = %s AND last_names.nationality = %s 
-ORDER BY RAND() LIMIT 1;
+SELECT GET_FULL_NAME(%s, %s);
