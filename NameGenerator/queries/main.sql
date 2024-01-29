ba9d4d49bb858d95f17eb27cfd3c53f4a75f5f19
karkraz <rochadcmarcos@gmail.com>
2024-01-29 14:52:55 -0300

    refactor: select full name

--- NameGenerator/queries/main.sql
+++ NameGenerator/queries/main.sql
@@ -10,3 +10,31 @@ CREATE TABLE last_names(
     nationality VARCHAR(100)
 );
 
+
+DELIMITER $
+
+CREATE FUNCTION GET_FULL_NAME(FIRST_NATIONALITY VARCHAR(100), LAST_NATIONALITY VARCHAR(100))
+RETURNS VARCHAR(200) 
+DETERMINISTIC
+BEGIN
+	DECLARE first_name VARCHAR(100);
+	DECLARE last_name VARCHAR(100);
+
+    SELECT first_names.value        
+    FROM first_names
+    WHERE first_names.nationality = FIRST_NATIONALITY 
+    ORDER BY RAND() LIMIT 1
+    INTO first_name;
+	
+    SELECT last_names.value
+    FROM last_names
+    WHERE last_names.nationality = LAST_NATIONALITY
+    ORDER BY RAND() LIMIT 1
+    INTO last_name;
+   	
+    RETURN CONCAT(first_name, ' ', last_name);
+END;
+$
+
+
+DELIMITER ;
